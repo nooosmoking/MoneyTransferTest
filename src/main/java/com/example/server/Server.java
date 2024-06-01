06768617ddc3921ce77d3c290c69f706f0657d6d
kaleighh <kaleighh@s21.ru>
2024-06-02 01:57:52 +0300

    will rework it

--- src/main/java/com/example/server/Server.java
+++ src/main/java/com/example/server/Server.java
@@ -44,44 +44,45 @@ public class Server {
         while (true) {
             try {
                 Socket client = server.accept();
-                new ClientThread(client).start();
+                new ClientThread(client).run();
             } catch (IOException e) {
                 logger.error("Error while connecting client");
             }
         }
     }
 
-    private class ClientThread extends Thread {
-        private final Socket clientSocket;
+    private class ClientThread {
         private final DataOutputStream out;
         private final BufferedReader in;
 
         public ClientThread(Socket clientSocket) throws IOException {
-            this.clientSocket = clientSocket;
             this.out = new DataOutputStream(clientSocket.getOutputStream());
             this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             logger.info("New client connected");
         }
 
-        @Override
         public void run() {
             while (true) {
                 handleHttpRequest();
             }
-
         }
 
         private void handleHttpRequest() {
             try {
+                String body = readBody();
                 String[] headers = parseHeader();
-                String body = readBody(in);
-                getMethod(headers, body);
+
+                if (body.isEmpty()) {
+                    sendErrorMessage();
+                } else {
+                    getMethod(headers, body);
+                }
             } catch (IOException ex) {
                 System.err.println("Error while handling http request.");
                 ex.printStackTrace();
             } catch (InvalidRequestHeaderException ex) {
                 System.err.println(ex.getMessage());
-            } catch (NullPointerException ignored){
+            } catch (NullPointerException ignored) {
             }
         }
 
@@ -91,13 +92,13 @@ public class Server {
             try {
                 String method = parts[0];
                 String[] uri = parts[1].split("/");
-                if (uri[0].equals(url) ||uri[0].isEmpty() ) {
+                if (uri[0].equals(url) || uri[0].isEmpty()) {
                     return new String[]{method, uri[1]};
                 } else {
                     throw new InvalidRequestHeaderException("Unknown request URL.");
                 }
             } catch (IndexOutOfBoundsException | NullPointerException ex) {
-                sendErrorMessage(out);
+                sendErrorMessage();
                 throw new InvalidRequestHeaderException("Invalid http request headers.");
             }
         }
@@ -114,11 +115,11 @@ public class Server {
             } else if (method.equalsIgnoreCase("POST") && path.equals("signin")) {
                 bankController.signin(new SigninRequest(body), out);
             } else {
-                sendErrorMessage(out);
+                sendErrorMessage();
             }
         }
 
-        private String readBody(BufferedReader in) throws IOException {
+        private String readBody() throws IOException {
             StringBuilder bodyBuilder = new StringBuilder();
             int b;
             while ((b = in.read()) != -1) {
@@ -127,8 +128,8 @@ public class Server {
             return bodyBuilder.toString();
         }
 
-        private void sendErrorMessage(DataOutputStream out) throws IOException {
-            out.writeUTF("404 Not Found");
+        private void sendErrorMessage() throws IOException {
+            out.writeUTF("400 Bad Request");
         }
     }
 
