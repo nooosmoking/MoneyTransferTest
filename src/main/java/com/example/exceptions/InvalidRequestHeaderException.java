f5e05795ce935e634530d862a7cf9144d1286191
kaleighh <kaleighh@s21.ru>
2024-06-03 21:10:41 +0300

    add exceptions

--- src/main/java/com/example/exceptions/InvalidRequestHeaderException.java
+++
@@ -1,7 +0,0 @@
-package com.example.exceptions;
-
-public class InvalidRequestHeaderException extends Exception {
-    public InvalidRequestHeaderException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/server/Server.java
+++ src/main/java/com/example/server/Server.java
@@ -1,7 +1,9 @@
 package com.example.server;
 
 import com.example.controllers.BankController;
-import com.example.exceptions.InvalidRequestHeaderException;
+import com.example.exceptions.InvalidRequestException;
+import com.example.exceptions.MethodNotAllowedException;
+import com.example.exceptions.ResourceNotFoundException;
 import com.example.models.SigninRequest;
 import com.example.models.SignupRequest;
 import com.example.models.TransferRequest;
@@ -15,7 +17,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -33,8 +34,8 @@ public class Server {
     }
 
     public void setAddress(String[] address) {
-        try {
-            this.server = new ServerSocket(Integer.parseInt(address[1]));
+        int port = Integer.parseInt(address[1]);
+        try (this.server = new ServerSocket(port)) {
             this.url = address[0];
         } catch (IOException e) {
             logger.error("Error while starting server.");
@@ -58,6 +59,8 @@ public class Server {
         private final DataOutputStream out;
         private final BufferedReader in;
         private Map<String, String> headers;
+        private String body;
+
         public ClientThread(Socket clientSocket) throws IOException {
             this.out = new DataOutputStream(clientSocket.getOutputStream());
             this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -72,24 +75,27 @@ public class Server {
 
         private void handleHttpRequest() {
             try {
-                parseStartLine();
-                parseHeader();
-                String body = readBody();
-                if (body.isEmpty()) {
-                    sendErrorMessage();
-                } else {
-                    implementMethod(headers, body);
+                try {
+                    parseStartLine();
+                    parseHeader();
+                    body = readBody();
+                    implementMethod();
+                } catch (InvalidRequestException ex) {
+                    System.err.println(ex.getMessage());
+                    sendErrorResponse(400, "Bad Request", "{\"message\": \"" + ex.getMessage() + "\"}");
+                } catch (ResourceNotFoundException ex){
+                    System.err.println(ex.getMessage());
+                    sendErrorResponse(404, "Not Found", "{\"message\": \"" + ex.getMessage() + "\"}");
+                } catch (MethodNotAllowedException ex){
+                    System.err.println(ex.getMessage());
+                    sendErrorResponse(405, "Method Not Allowed ", "{\"message\": \"" + ex.getMessage() + "\"}");
                 }
             } catch (IOException ex) {
                 System.err.println("Error while handling http request.");
-                ex.printStackTrace();
-            } catch (InvalidRequestHeaderException ex) {
-                System.err.println(ex.getMessage());
-            } catch (NullPointerException ignored) {
             }
         }
 
-        private void parseStartLine() throws IOException, InvalidRequestHeaderException {
+        private void parseStartLine() throws IOException, InvalidRequestException {
             headers = new HashMap<>();
             String request = in.readLine();
             String[] parts = request.split(" ");
@@ -99,29 +105,51 @@ public class Server {
                 if (uri[0].equals(url) || uri[0].isEmpty()) {
                     headers.put("path", uri[1]);
                 } else {
-                    throw new InvalidRequestHeaderException("Unknown request URL.");
+                    throw new InvalidRequestException("Unknown request URL.");
                 }
             } catch (IndexOutOfBoundsException | NullPointerException ex) {
-                sendErrorMessage();
-                throw new InvalidRequestHeaderException("Invalid http request start line.");
+                throw new InvalidRequestException("Invalid http request start line.");
             }
         }
 
-        private void parseHeader(){
+        private void parseHeader() throws IOException, InvalidRequestException {
+            String request;
+            while (!(request = in.readLine()).isEmpty()) {
 
+                String[] parts = request.split(": ");
+                try {
+                    headers.put(parts[0], parts[1]);
+                    String[] uri = parts[1].split("/");
+                } catch (IndexOutOfBoundsException | NullPointerException ex) {
+                    throw new InvalidRequestException("Invalid http request start line.");
+                }
+            }
         }
 
-        private void implementMethod(String body) throws IOException {
-            if (method.equalsIgnoreCase("POST") && path.equals("money")) {
-                bankController.transferMoney(new TransferRequest(body), out);
-            } else if (method.equalsIgnoreCase("GET") && path.equals("money")) {
-                bankController.getBalance("authToken", out);
-            } else if (method.equalsIgnoreCase("POST") && path.equals("signup")) {
-                bankController.signup(new SignupRequest(body), out);
-            } else if (method.equalsIgnoreCase("POST") && path.equals("signin")) {
-                bankController.signin(new SigninRequest(body), out);
+        private void implementMethod() throws IOException, InvalidRequestException, ResourceNotFoundException, MethodNotAllowedException {
+            String method = headers.get("method");
+            String path = headers.get("path");
+            if (method.equalsIgnoreCase("GET") ) {
+                if(path.equals("money")) {
+                    bankController.getBalance("authToken", out);
+                } else {
+                    throw new ResourceNotFoundException("Resource not found \""+path+"\"");
+                }
+            } else if ( method.equalsIgnoreCase("POST")){
+                if(!body.isEmpty()) {
+                    if (path.equals("money")) {
+                        bankController.transferMoney(new TransferRequest(body), out);
+                    } else if (path.equals("signup")) {
+                        bankController.signup(new SignupRequest(body), out);
+                    } else if (path.equals("signin")) {
+                        bankController.signin(new SigninRequest(body), out);
+                    } else {
+                        throw new ResourceNotFoundException("Resource not found \""+path+"\"");                    }
+                } else {
+                    throw new InvalidRequestException("Body is empty");
+                }
             } else {
-                sendErrorMessage();
+                throw new MethodNotAllowedException("Method " + method + " not allowed");
             }
         }
 
@@ -134,8 +162,13 @@ public class Server {
             return bodyBuilder.toString();
         }
 
-        private void sendErrorMessage() throws IOException {
-            out.writeUTF("400 Bad Request");
+        private void sendErrorResponse(int status, String statusMessage, String body
+                                  ) throws IOException {
+            String response = "HTTP/1.1 " + status + " " + statusMessage + "\r\n" + "Content-Type: application/json\r\n" +
+                    "Content-Length: " + body.length() + "\r\n\r\n" +
+                    body;
+
+            out.writeUTF(response);
         }
     }
 
