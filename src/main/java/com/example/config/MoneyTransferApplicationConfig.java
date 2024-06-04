3f1835a175018dbf2e7afe93d5a373d373ca6160
kaleighh <kaleighh@s21.ru>
2024-06-04 19:34:54 +0300

    add password encoder

--- src/main/java/com/example/config/MoneyTransferApplicationConfig.java
+++ src/main/java/com/example/config/MoneyTransferApplicationConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @PropertySource("classpath:db.properties")
@@ -33,5 +35,10 @@ public class MoneyTransferApplicationConfig {
     @Bean
     public HikariDataSource hikariDataSource() {
         return new HikariDataSource(hikariConfig());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/services/AuthServiceImpl.java
+++ src/main/java/com/example/services/AuthServiceImpl.java
@@ -1,7 +1,18 @@
 package com.example.services;
 
+import com.example.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService{
+    private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthServiceImpl(UsersRepository usersRepository, PasswordEncoder passwordEncoder) {
+        this.usersRepository = usersRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 }
